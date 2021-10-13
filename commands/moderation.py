9ae57c83ab08c3c86d77be997b09b99322f29b6f
losowy <arandomone.businnes@gmail.com>
2021-10-13 23:01:42 +0200

    Add slash commands to most moderation commands

--- commands/moderation.py
+++ commands/moderation.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord_slash import cog_ext
 
 class Moderation(commands.Cog):
 
@@ -11,40 +12,43 @@ class Moderation(commands.Cog):
 		print('Moderation commands loaded.')
 	
 	#region kick command
-	@commands.command(name='kick')
+	@cog_ext.cog_slash(name='kick', description='Kick a server member.', guild_ids=[897602500440498218])
 	@commands.has_permissions(kick_members=True)
-	async def _kick(self, ctx, user: discord.Member, *, reason=None):
-		if user == ctx.author:
+	async def _kick(self, ctx, member: discord.Member, *, reason=None):
+		if member == ctx.author:
 			await ctx.send('You can\'t kick yourself!')
 		else:
-			await user.kick(reason=reason)
+			await member.kick(reason=reason)
 	#endregion
 
 	#region ban command
-	@commands.command(name='ban')
+	@cog_ext.cog_slash(name='ban', description='Ban a server member.', guild_ids=[897602500440498218])
 	@commands.has_permissions(ban_members=True)
-	async def _ban(self, ctx, user: discord.Member, *, reason=None):
-		await user.ban(reason=reason)
+	async def _ban(self, ctx, member: discord.Member, *, reason=None):
+		if member == ctx.author:
+			await ctx.send('You can\'t ban yourself!')
+		else:
+			await member.ban(reason=reason)
 	#endregion
 
 	#region clear command
-	@commands.command(name='clear')
+	@cog_ext.cog_slash(name='clear', description='Clear an amount of messages from the current channel.', guild_ids=[897602500440498218])
 	@commands.has_permissions(manage_messages=True)
 	async def _clear(self, ctx, amount=1):
-		await ctx.channel.purge(limit=amount+1)
+		await ctx.channel.purge(limit=int(amount)+1)
 		await ctx.send(f'Successfully deleted {amount} messages!', delete_after=2)
 	#endregion
 
 	#region nick command
-	@commands.command(name='nick')
+	@cog_ext.cog_slash(name='nick', description='Give a server member a new nickname.', guild_ids=[897602500440498218])
 	@commands.has_permissions(manage_nicknames=True)
-	async def _nick(self, ctx, user: discord.Member, *, nickname=None):
+	async def _nick(self, ctx, user: discord.Member, *, nickname):
 		await user.edit(nick=nickname)
 		await ctx.send('Successfully changed nickname for ' + str(user.mention))
 	#endregion
 
 	#region user-info command
-	@commands.command(name='user-info')
+	@cog_ext.cog_slash(name='user-info', description='Get information on a server member.', guild_ids=[897602500440498218])
 	@commands.has_permissions()
 	async def _userinfo(self, ctx, user: discord.Member):
 		embed=discord.Embed(title="User Info", description=user, color=0x5555ff)
@@ -57,7 +61,7 @@ class Moderation(commands.Cog):
 	#endregion
 
 	#region help command
-	@commands.command(name='help')
+	@cog_ext.cog_slash(name='help', description='Get a list of bot commands.', guild_ids=[897602500440498218])
 	@commands.has_permissions()
 	async def _help(self, ctx):
 		embed=discord.Embed(title="Help", description="List of Commands", color=0x00ff00)
