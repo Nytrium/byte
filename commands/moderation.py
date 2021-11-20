46a7052e885e79f136db8bff92880c7fa29584e6
losowy <arandomone.businnes@gmail.com>
2021-11-20 13:56:59 +0100

    Testing...

--- commands/moderation.py
+++ commands/moderation.py
@@ -80,10 +80,13 @@ class Moderation(commands.Cog):
 	#endregion
 
 	#region mute command
-	@commands.has_permissions(administrator=True, manage_permissions=True, mute_members=True, manage_roles=True)
+	#@commands.has_permissions(manage_permissions=True, mute_members=True, manage_roles=True)
 	@cog_ext.cog_slash(name='mute', description='Mute a server member.', guild_ids=guildIDs)
 	async def _mute(self, ctx, member: discord.Member):
-		print(member.roles)
+		if ctx.message.author.has_permissions(manage_permissions=True, mute_members=True, manage_roles=True) or ctx.message.author.has_permissions(administrator=True):
+			print(member.roles)
+		else:
+			pass
 	#endregion
 
 def setup(client):
