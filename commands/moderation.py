a4890f09610b6a0b425434d73fe0bf20eb6579a4
losowy <arandomone.businnes@gmail.com>
2021-12-03 20:00:55 +0100

    pp

--- commands/moderation.py
+++ commands/moderation.py
@@ -75,16 +75,16 @@ class Moderation(commands.Cog):
 	async def _userinfo(self, ctx, user: discord.Member):
 		embed=discord.Embed(title="User Info", description=user, color=0x5555ff)
 		embed.set_thumbnail(url=user.avatar_url)
-		embed.add_field(name="Joined Server At", value=str(user.joined_at)[:-10], inline=True)
-		embed.add_field(name="Joined Discord At", value=str(user.created_at)[:-10], inline=True)
+		embed.add_field(name="Joined Server At", value=str(user.joined_at)[:-10], inline=False)
+		embed.add_field(name="Joined Discord At", value=str(user.created_at)[:-10], inline=False)
 		if user.id == 792162727907950652:
-			embed.add_field(name="Height", value="short", inline=True)
+			embed.add_field(name="Height", value="short", inline=False)
 		elif user.id == 711179491132571689:
-			embed.add_field(name="Height", value="Luigi", inline=True)
+			embed.add_field(name="Height", value="Luigi", inline=False)
 		elif user.id == 429355349829615627:
-			embed.add_field(name="Height", value="super ultra mega tall man person cool epic", inline=True)
+			embed.add_field(name="Height", value="super ultra mega tall man person cool epic", inline=False)
 		elif user.id == 429083015025786880:
-			embed.add_field(name="Height", value="literal goomba", inline=True)
+			embed.add_field(name="Height", value="literal goomba", inline=False)
 		else:
 			pass
 		embed.set_footer(text="Command run by " + str(ctx.author))
