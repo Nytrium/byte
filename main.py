9ea1b512b24ab0a048daa9c6d93adaf3c480ab02
losowy <arandomone.businnes@gmail.com>
2021-12-24 12:22:29 +0100

    lol forgot about this

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import discord # pip install discord.py
 from discord.ext import commands # pip install discord.py
-from discord_slash import SlashCommand # pip install discord-py-slash-command
 import os
 
 client = commands.Bot(command_prefix='$', help_command=None, intents=discord.Intents.all())
-slash = SlashCommand(client, sync_commands=True)
 
 @client.event
 async def on_ready():
